980b14ee1239cb1bb5ba14ecfd5199f01cf400f6
Romika Chaudhary <152133936+Romsh1@users.noreply.github.com>
2024-10-04 16:56:50 -0400

    Connection to MongoDb Atlas and products displayed in the browser

---
+++ Assignment 2/tests/test_app.py
@@ -0,0 +1,16 @@
+from app import flask_app
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        #Seting up the test client
+        self.app = flask_app.test_client()
+        self.app.testing = True
+
+    def test_index_1(self):
+        #Sending a GET request to the "/" route
+        response = self.app.get("/")
+
+        #Checking if the response is 200 OK
+        self.assertEqual(first=response.status_code, second=200)
