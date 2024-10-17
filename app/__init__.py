a74f6ea5a40947bdebee06949cf5d7d1e20d4a77
Romika <romika2002chau.21@gmail.com>
2024-10-17 15:59:54 -0400

    Added a .env file to secure configuration management

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from pymongo import MongoClient
+from dotenv import load_dotenv 
+import os
 
+load_dotenv()
 flask_app = Flask(__name__)
 
+db_username = os.environ["MONGODB_USERNAME"]
+db_password = os.environ["MONGODB_PASSWORD"]
+
+# print(db_username)
+# print(db_password)
+
 #MongoDB Atlas Connection
-client = MongoClient("mongodb+srv://root:R6cc5hdGcljx8FIJ@cluster0.znx2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient(f"mongodb+srv{db_username}:{db_password}//@cluster0.znx2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client.shop_db  
 products_collection = db.products  
 
