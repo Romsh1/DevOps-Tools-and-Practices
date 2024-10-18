623d3c2cb48577b5ceb6edc8a0a6c2b84962e884
Romika <romika2002chau.21@gmail.com>
2024-10-18 13:42:39 -0400

    Correction on the url while passing username and password

--- app/__init__.py
+++ app/__init__.py
@@ -9,11 +9,11 @@ flask_app = Flask(__name__)
 db_username = os.environ["MONGODB_USERNAME"]
 db_password = os.environ["MONGODB_PASSWORD"]
 
-# print(db_username)
-# print(db_password)
+print(db_username)
+print(db_password)
 
 #MongoDB Atlas Connection
-client = MongoClient(f"mongodb+srv{db_username}:{db_password}//@cluster0.znx2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient(f"mongodb+srv://{db_username}:{db_password}@cluster0.znx2q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client.shop_db  
 products_collection = db.products  
 
